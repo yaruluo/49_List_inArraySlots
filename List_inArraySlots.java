9832c102d1c88c7f3f35aea5a8857bf4bee363a3
yevagorbachev <ygorbachev00@stuy.edu>
2019-03-11 21:07:50 -0400

    Updated User and List_iAS to 51 skeletons

--- List_inArraySlots.java
+++ List_inArraySlots.java
@@ -1,147 +1,84 @@
 /**
- * Implement a list of integer elements, including both data and operations.
+  Implement a list of integer elements, including
+  both data and operations.
  */
 
-import java.util.*;
+/*Note from Yevgeniy:
+  Each element i has integer, double, and String 'components'.
+  typeOfElements stores which of the components is relevant.
+*/
+
 public class List_inArraySlots {
 
-  private int[] elements; // container for the elements of the list
-  private int filledElements; // the number of elements in this list
-
-  private static final int INITIAL_CAPACITY = 10;
-
-  /**
-   * Construct an empty list with a small initial capacity.
-   */
-  public List_inArraySlots() {
-    elements = new int[INITIAL_CAPACITY];
-    // filledElements has been initialized to the desired value, 0
-  }
-
-  /**
-   * @return the number of elements in this list
-   */
-  public int size() {
-    return filledElements;
-  }
-
-  /**
-   * @return a string representation of this list, in [a,b,c,] format
-   */
-  public String toString() {
-    String result = "[";
-    for (int elemIndex = 0; elemIndex < filledElements; elemIndex++)
-      result += elements[elemIndex] + ",";
-    return result + "]";
-  }
-
-  /**
-   * Appends @value to the end of this list.
-   * 
-   * @return true, in keeping with conventions yet to be discussed
-   */
-  public boolean add(int value) {
-    // expand if necessary
-    if (filledElements == elements.length)
-      expand();
-
-    elements[filledElements] = value;
-    filledElements++;
-    // idiomatic version: elements[ filledElements++] = value;
-    return true;
-  }
-
-  /**
-   * Double the capacity of the List_inArraySlots, preserving existing data.
-   */
-  private void expand() {
-    System.out.println("expand... (for debugging)");
-    /*
-     * S.O.P. rules for debugging: Working methods should be silent. But during
-     * development, the programmer must verify that this method is called when that
-     * is appropriate. So test using the println(), then comment it out.
-     */
-    int[] bigger = new int[elements.length * 2];
-    for (int elemIndex = 0; elemIndex < filledElements; elemIndex++)
-      bigger[elemIndex] = elements[elemIndex];
-    elements = bigger;
-  }
-
-  /**
-   * accessor
-   * 
-   * @return element @index from this list precondition: @index is within the
-   *         bounds of the array. (Having warned the user about this precondition,
-   *         you should NOT complicate your code to check whether user violated
-   *         the condition.)
-   */
-  public int get(int index) {
-    return elements[index];
-  }
-
-  /**
-   * Set value at @index to @newValue
-   * 
-   * @return old value at @index
-   * @precondition: @index is within the bounds of this list.
-   */
-  public int set(int index, int newValue) {
-    int old = elements[index];
-    elements[index] = newValue;
-    return old;
+    private int[]    intElements;
+    private double[] doubleElements;
+    private String[] stringElements;
+    private int filledElements; // the number of elements in this list
     
-  }
+    /* type identifier for each element
+       That is, typeOfElements[i] == 0 means element i is an integer;
+                                     1 means element i is a double;
+                                     2 means element i is a String.
+        Optional extra education in programming (not comp sci):
+            replace these "magic numbers" with an "enumerated type".
+     */
 
-  /**
-   * Insert @value at position @index in this list.
-   * 
-   * Shift the element currently at that position (if any) and any subsequent
-   * elements to the right (that is, increase the index associated with each).
-   */
-  public void add(int index, int value) {
-    int[] elements_new = new int[filledElements * 2];
-    //populate the first few elements
-    int i = 0;
-    for ( ; i < index; i++) {
-      elements_new[i] = elements[i];
-    }
-    elements_new[i] = value;
-    i++;
-    for ( ; i < filledElements + 1; i++) {
-      elements_new[i] = elements[i - 1];
-    }
-    elements = elements_new;
-    filledElements++;
-  }
+    private int[] typeOfElements;
 
-  //expand the array by 1
-  //fill the first few elements, stop once the inserting index is reached
-  //successful, when i = index, loop ends.
-  //assign the value to the right index
-  //fill in the rest of the elements, stops at filledElements + 1
-  /**
-   * Remove the element at position @index in this list.
-   * 
-   * Shift any subsequent elements to the left (that is, decrease the index
-   * associated with each).
-   * 
-   * @return the value that was removed from the list
-   */
-  public int remove(int index) {
-    int[] elements_new = new int[filledElements - 1];
-    int i = 0;
-    // populate first few elements
-    for ( ; i < index; i++) {
-      elements_new[i] = elements[i];
-    }
-    int deletee = elements[i];
-    i++;
-    for ( ; i < filledElements; i++) {
-      elements_new[i - 1] = elements[i];
-    }
-    elements = elements_new;
-    filledElements--;
-    return deletee;
-  }
+    private static final int INITIAL_CAPACITY = 10;
 
+    /**
+      Construct an empty list with a small initial capacity.
+     */
+    public List_inArraySlots() {
+      intElements = new int[INITIAL_CAPACITY];
+      doubleElements = new double[INITIAL_CAPACITY];
+      stringElements = new String[INITIAL_CAPACITY];
+      filledElements = 0;
+    }
+
+
+    /**
+      @return the number of elements in this list
+     */
+    public int size() {
+      return filledElements;
+    }
+
+
+     /**
+       @return a string representation of this list,
+       in [a,b,c,] format
+      */
+    // public String toString() {
+    // }
+      //For each element in typeOfElements:
+      //return the index of the correct one of the three arrays that store elements.
+
+    /**
+      Appends @value to the end of this list.
+
+      @return true, in keeping with conventions yet to be discussed
+     */
+     // public boolean add( int type   // same meaning as in typeOfElements
+                       // , int intValue
+                       // , double doubleValue
+                       // , String stringValue
+                       // ) {
+     // }
+
+
+    /**
+      Double the capacity of the List_inArraySlots,
+      preserving existing data.
+     */
+     // private void expand() {
+        // System.out.println( "expand... (for debugging)");
+           // /* S.O.P. rules for debugging:
+              // Working methods should be silent. But during
+              // development, the programmer must verify that
+              // this method is called when that is appropriate.
+              // So test using the println(), then comment it out.
+              // */
+     // }
 }
--- UserOfList.java
+++ UserOfList.java
@@ -2,96 +2,35 @@
  Test list features.
  */
 public class UserOfList {
-    private static List_inArraySlots list;
-
     public static void main( String[] args ) {
-        list = new List_inArraySlots();
+        List_inArraySlots list = new List_inArraySlots();
 
-        System.out.println( "number of elements: " + list.size() );
-        System.out.println( "empty list: " + list);
+        // System.out.println( "number of elements: " + list.size() );
+        // System.out.println( "empty list: " + list);
 
-        /* Populate the list with elements, but with a small enough
-           number that we expect no invocation of expand().
-        */
-        int elemIndex;
-        for( elemIndex = 0; elemIndex < 5; elemIndex++ ) {
-            list.add( -elemIndex); // differs from index, but similar
-            System.out.println( "number of elements: " + list.size() );
-        }
-        System.out.println("initial population of " + list.size() + " elements:");
-        System.out.println( list + System.lineSeparator());
+        // // Populate the list with diverse elements.
+        // list.add( 2, -1, -2.0,  "important");
+        // System.out.println( "number of elements: " + list.size() );
+        // System.out.println( "added string: " + list);
 
-        // Add enough elements that expansion is expected
-        for( ; elemIndex < 15; elemIndex++ ) {
+        // list.add( 0, 17, -2.0,  "junk");
+        // System.out.println( "number of elements: " + list.size() );
+        // System.out.println( "added int: " + list);
 
-            if( elemIndex == 10) System.out.println( "expansion expected");
+        // list.add( 1, -1, 1.618, "junk");
+        // System.out.println( "number of elements: " + list.size() );
+        // System.out.println( "added double: " + list);
 
-            list.add( -elemIndex);
-            System.out.println( "number of elements: " + list.size() );
-        }
-        System.out.println("result of second population: " + list.size() + " elements:");
-        System.out.println( list + System.lineSeparator());
+        // // Add enough elements that expansion is expected
+        // for( ; elemIndex < 15; elemIndex++ ) {
 
-        // Trust no one.
-        for( ; elemIndex < 35; elemIndex++ )
-            list.add( -elemIndex);
-        System.out.println("after second expansion: " + list.size() + " elements:");
-        System.out.println( list + System.lineSeparator());
-        
-        // test accessor
-        System.out.println( "sample elements from list:");
-        for( elemIndex = 1; elemIndex < list.size(); elemIndex *= 2 ) {
-            System.out.println( "element " + elemIndex + ": "
-                              + list.get( elemIndex)
-                              );
-        }
+            // if( elemIndex == 10) System.out.println( "expansion expected");
 
-        // test set
-        setTest(  8);
-        setTest( 16);
-        System.out.println();
-
-        // test adding at a specified position
-        addAtTest( 0, 29); // beginning of the list
-
-        // end of the list using the new add method
-        addAtTest( list.size(), 17);
-
-        addAtTest( 2, 19); // middle of a small list
-
-        // force an expansion
-        addAtTest( 2, 23);
-        addAtTest( 2, 23);
-        addAtTest( 2, 23);
-
-        System.out.println("removing value " + list.remove( 6)
-                          + ", leaving " + list.size() + " elements:");
-        System.out.println( list);
-        System.out.println(
-            "expecting:" + System.lineSeparator()
-          + "[29,0,23,23,23,19, NO -1 HERE! -2,-3...]");
-    }  // end of main()
-
-    private static void setTest( int modifyAt
-                              ) {
-        System.out.println(
-            "changed element " + modifyAt + " from "
-          + list.set( modifyAt, modifyAt + 1000) + " to "
-          + list.get( modifyAt)
-          );
-     }
-
-     private static void addAtTest( int addAt
-                                 , int value
-                                 ) {
-        list.add( addAt, value);
-        System.out.println(
-            "insert " + value
-          + " at position " + addAt
-          + ", resulting in "  + list.size() + " elements:"
-          + System.lineSeparator()
-          + list
-          + System.lineSeparator()
-        );
+            // list.add( 0, -i, -2.0,  "junk");
+            // System.out.println( "number of elements: " + list.size() );
+        // }
+        // System.out.println("result of expanded list: "
+             // + list.size() + " elements:");
+        // System.out.println( list + System.lineSeparator());
     }
 }
