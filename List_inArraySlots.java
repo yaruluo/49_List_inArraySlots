9e8cbe998a68d45e9ca529efdc15478e245d0a99
yevagorbachev <ygorbachev00@stuy.edu>
2019-03-12 05:51:38 -0400

    reverted to original List_iAS

--- List_inArraySlots.java
+++ List_inArraySlots.java
@@ -55,8 +55,34 @@ public class List_inArraySlots {
       //For each element in typeOfElements:
       //return the index of the correct one of the three arrays that store elements.
 
+<<<<<<< HEAD
     /**
       Appends @value to the end of this list.
+=======
+  /**
+   * Insert @value at position @index in this list.
+   * 
+   * Shift the element currently at that position (if any) and any subsequent
+   * elements to the right (that is, increase the index associated with each).
+   */
+  public void add(int index, int value) {
+    int[] elements_new = new int[1 + filledElements];
+    //populate the first few elements
+    int i = 0;
+    for ( ; i < index; i++) {
+      elements_new[i] = elements[i];
+    }
+    System.out.println(Arrays.toString(elements_new));
+    System.out.println(Arrays.toString(elements));
+    elements_new[i] = value;
+    i++;
+    for ( ; i < filledElements + 1; i++) {
+      elements_new[i] = elements[i - 1];
+    }
+    elements = elements_new;
+    filledElements++;
+  }
+>>>>>>> parent of 1887a01... Updated List_iAS.add(int, int) to share behavior with yaruluo's add(int,int)
 
       @return true, in keeping with conventions yet to be discussed
      */
