13bcccad2e23957ff0175749033aca73efd4783c
yevagorbachev <ygorbachev00@stuy.edu>
2019-03-12 05:59:41 -0400

    K

--- List_inArraySlots.java
+++ List_inArraySlots.java
@@ -1,60 +1,85 @@
 /**
-  Implement a list of integer elements, including
-  both data and operations.
+ * Implement a list of integer elements, including both data and operations.
  */
 
-/*Note from Yevgeniy:
-  Each element i has integer, double, and String 'components'.
-  typeOfElements stores which of the components is relevant.
-*/
-
+import java.util.*;
 public class List_inArraySlots {
 
-    private int[]    intElements;
-    private double[] doubleElements;
-    private String[] stringElements;
-    private int filledElements; // the number of elements in this list
-    
-    /* type identifier for each element
-       That is, typeOfElements[i] == 0 means element i is an integer;
-                                     1 means element i is a double;
-                                     2 means element i is a String.
-        Optional extra education in programming (not comp sci):
-            replace these "magic numbers" with an "enumerated type".
+  private int[] elements; // container for the elements of the list
+  private int filledElements; // the number of elements in this list
+
+  private static final int INITIAL_CAPACITY = 10;
+
+  /**
+   * Construct an empty list with a small initial capacity.
+   */
+  public List_inArraySlots() {
+    elements = new int[INITIAL_CAPACITY];
+    // filledElements has been initialized to the desired value, 0
+  }
+
+  /**
+   * @return the number of elements in this list
+   */
+  public int size() {
+    return filledElements;
+  }
+
+  /**
+   * @return a string representation of this list, in [a,b,c,] format
+   */
+  public String toString() {
+    String result = "[";
+    for (int elemIndex = 0; elemIndex < filledElements; elemIndex++)
+      result += elements[elemIndex] + ",";
+    return result + "]";
+  }
+
+  /**
+   * Appends @value to the end of this list.
+   * 
+   * @return true, in keeping with conventions yet to be discussed
+   */
+  public boolean add(int value) {
+    // expand if necessary
+    if (filledElements == elements.length)
+      expand();
+
+    elements[filledElements] = value;
+    filledElements++;
+    // idiomatic version: elements[ filledElements++] = value;
+    return true;
+  }
+
+  /**
+   * Double the capacity of the List_inArraySlots, preserving existing data.
+   */
+  private void expand() {
+    System.out.println("expand... (for debugging)");
+    /*
+     * S.O.P. rules for debugging: Working methods should be silent. But during
+     * development, the programmer must verify that this method is called when that
+     * is appropriate. So test using the println(), then comment it out.
      */
+    int[] bigger = new int[elements.length * 2];
+    for (int elemIndex = 0; elemIndex < filledElements; elemIndex++)
+      bigger[elemIndex] = elements[elemIndex];
+    elements = bigger;
+  }
 
-    private int[] typeOfElements;
+  /**
+   * accessor
+   * 
+   * @return element @index from this list precondition: @index is within the
+   *         bounds of the array. (Having warned the user about this precondition,
+   *         you should NOT complicate your code to check whether user violated
+   *         the condition.)
+   */
+  public int get(int index) {
+    return elements[index];
+  }
 
-    private static final int INITIAL_CAPACITY = 10;
-
-    /**
-      Construct an empty list with a small initial capacity.
-     */
-    public List_inArraySlots() {
-      intElements = new int[INITIAL_CAPACITY];
-      doubleElements = new double[INITIAL_CAPACITY];
-      stringElements = new String[INITIAL_CAPACITY];
-      filledElements = 0;
-    }
-
-
-    /**
-      @return the number of elements in this list
-     */
-    public int size() {
-      return filledElements;
-    }
-
-
-     /**
-       @return a string representation of this list,
-       in [a,b,c,] format
-      */
-    // public String toString() {
-    // }
-      //For each element in typeOfElements:
-      //return the index of the correct one of the three arrays that store elements.
-
+<<<<<<< HEAD
 <<<<<<< HEAD
     /**
       Appends @value to the end of this list.
@@ -83,28 +108,71 @@ public class List_inArraySlots {
     filledElements++;
   }
 >>>>>>> parent of 1887a01... Updated List_iAS.add(int, int) to share behavior with yaruluo's add(int,int)
+=======
+  /**
+   * Set value at @index to @newValue
+   * 
+   * @return old value at @index
+   * @precondition: @index is within the bounds of this list.
+   */
+  public int set(int index, int newValue) {
+    int old = elements[index];
+    elements[index] = newValue;
+    return old;
+    
+  }
+>>>>>>> parent of 9832c10... Updated User and List_iAS to 51 skeletons
 
-      @return true, in keeping with conventions yet to be discussed
-     */
-     // public boolean add( int type   // same meaning as in typeOfElements
-                       // , int intValue
-                       // , double doubleValue
-                       // , String stringValue
-                       // ) {
-     // }
+  /**
+   * Insert @value at position @index in this list.
+   * 
+   * Shift the element currently at that position (if any) and any subsequent
+   * elements to the right (that is, increase the index associated with each).
+   */
+  public void add(int index, int value) {
+    int[] elements_new = new int[filledElements * 2];
+    //populate the first few elements
+    int i = 0;
+    for ( ; i < index; i++) {
+      elements_new[i] = elements[i];
+    }
+    elements_new[i] = value;
+    i++;
+    for ( ; i < filledElements + 1; i++) {
+      elements_new[i] = elements[i - 1];
+    }
+    elements = elements_new;
+    filledElements++;
+  }
 
+  //expand the array by 1
+  //fill the first few elements, stop once the inserting index is reached
+  //successful, when i = index, loop ends.
+  //assign the value to the right index
+  //fill in the rest of the elements, stops at filledElements + 1
+  /**
+   * Remove the element at position @index in this list.
+   * 
+   * Shift any subsequent elements to the left (that is, decrease the index
+   * associated with each).
+   * 
+   * @return the value that was removed from the list
+   */
+  public int remove(int index) {
+    int[] elements_new = new int[filledElements - 1];
+    int i = 0;
+    // populate first few elements
+    for ( ; i < index; i++) {
+      elements_new[i] = elements[i];
+    }
+    int deletee = elements[i];
+    i++;
+    for ( ; i < filledElements; i++) {
+      elements_new[i - 1] = elements[i];
+    }
+    elements = elements_new;
+    filledElements--;
+    return deletee;
+  }
 
-    /**
-      Double the capacity of the List_inArraySlots,
-      preserving existing data.
-     */
-     // private void expand() {
-        // System.out.println( "expand... (for debugging)");
-           // /* S.O.P. rules for debugging:
-              // Working methods should be silent. But during
-              // development, the programmer must verify that
-              // this method is called when that is appropriate.
-              // So test using the println(), then comment it out.
-              // */
-     // }
 }
